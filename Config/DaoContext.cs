bdefe9aa4b00a86807d1f283a1d2de0e6ddb68e1
Konstantin Trofimenko <konstantin@vreasy.com>
2020-04-15 13:05:10 +0300

    use args from command line to connect ot db

--- Config/DaoContext.cs
+++ Config/DaoContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using Sunrise.Database.Model;
 
@@ -5,12 +6,19 @@ namespace Sunrise.Config
 {
     public class DaoContext : DbContext 
     {
-    
         public DbSet<City> Cities {get;set;}   
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string config = "server=localhost;port=3306;database=dotnet;user=root;password=password;";
+            
+            string[] args = Environment.GetCommandLineArgs();
+            string server = args[1];
+            string port = args[2];
+            string database = args[3];
+            string user = args[4];
+            string password = args[5];
+
+            string config = String.Format("server={0};port={1};database={2};user={3};password={4};", server, port, database, user, password);
             optionsBuilder.UseMySql(config);
         }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -9,8 +10,11 @@ namespace Sunrise
 {
     class Program
     {
+        private static readonly int RequiredArgsSize = 5;
         static void Main(string[] args)
         {
+            ValidateArgs(args);
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .ConfigureLogging(factory => {
@@ -24,7 +28,14 @@ namespace Sunrise
                 .UseStartup<WebStartup>()
                 .Build();
             host.Run();
+        
         }
+
+        private static void ValidateArgs(string[] args) {
+            if(args.Length < RequiredArgsSize)
+                throw new ArgumentException(String.Format("Not enougth arguments. Required {0} but was {1}", RequiredArgsSize, args.Length));
+        }
+
         
     }
 }
