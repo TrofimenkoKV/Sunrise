8bdd390ac7162a0b16e40bc936a4668385dc5737
Konstantin Trofimenko <konstantin@vreasy.com>
2020-04-20 11:32:13 +0300

    added initial migration initial data

---
+++ Sunrise/Migrations/20200420082710_CityInitialData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sunrise.Migrations
+{
+    public partial class CityInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO `dotnet`.`city` (`city_name`, `longitude`, `latitude`) VALUES ('Lviv', '12.43', '12.32');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
